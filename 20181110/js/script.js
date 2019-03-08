902421632bb819b49faf2babf0d542d858545f71
qiaobing <qb20150806@163.com>
2019-03-08 20:31:03 +0800

    first commit

---
+++ 20181110/js/script.js
@@ -0,0 +1,9 @@
+alert('hello wold!');
+var username='kitty';
+var num1=100;
+var num2=200;
+document.write(username);
+document.write(num1+num2);
+var num=prompt('输入数字：');
+alert(num);
+// console.log{n1}在控制台输出日志
